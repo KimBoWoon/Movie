ad4ae5d95a90ac5dc49f909c25b40b2900cf3c66
bowoon <haioer@naver.com>
2025-12-13 14:17:19 +0900

    불필요 코드 제거

--- core/testing/src/main/java/com/bowoon/testing/repository/TestMainMenuRepository.kt
+++ core/testing/src/main/java/com/bowoon/testing/repository/TestMainMenuRepository.kt
@@ -3,22 +3,13 @@ package com.bowoon.testing.repository
 import androidx.annotation.VisibleForTesting
 import com.bowoon.data.repository.MainMenuRepository
 import com.bowoon.data.util.Synchronizer
-import com.bowoon.model.Movie
-import com.bowoon.testing.model.nowPlayingMoviesTestData
-import com.bowoon.testing.model.upcomingMoviesTestData
 import java.time.LocalDate
 
 class TestMainMenuRepository : MainMenuRepository {
     private var date = LocalDate.now()
 
-    //    override suspend fun syncWith(isForce: Boolean, notification: suspend () -> Unit): Boolean =
-//        LocalDate.now().minusDays(1).isAfter(date) || isForce
     override suspend fun syncWith(synchronizer: Synchronizer): Boolean =
         LocalDate.now().minusDays(1).isAfter(date)
-
-    override suspend fun getNowPlaying(): List<Movie> = nowPlayingMoviesTestData
-
-    override suspend fun getUpcomingMovies(): List<Movie> = upcomingMoviesTestData
 
     @VisibleForTesting
     fun setDate(date: LocalDate) {
