ee748433ba32c006d1b672cf4fe4572174acbcbb
bowoon <haioer@naver.com>
2025-12-23 15:10:17 +0900

    push.yml 테스트 코드 주석 처리

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,7 +25,7 @@ tasks.register("createReleaseNote") {
     releaseNote.writeText(
         text = logs.takeIf { it.isNotEmpty() }?.trimIndent() ?: "empty logs..."
     )
-    println(logs.takeIf { it.isNotEmpty() }?.trimIndent() ?: "empty logs...")
+//    println(logs.takeIf { it.isNotEmpty() }?.trimIndent() ?: "empty logs...")
 }
 
 dependencies {
