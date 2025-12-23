6a32516e8a98c748eb70093c8fd6c6d4032492ba
bowoon <haioer@naver.com>
2025-12-23 14:29:25 +0900

    push.yml 생성 및 테스트

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ tasks.register("createReleaseNote") {
     val logs = ByteArrayOutputStream().use { os ->
         exec {
             executable = "git"
-            args = listOf<String>("log", "--merges", "--oneline", "--pretty=format:(#%h) %cn %s", "release/${android.defaultConfig.versionName}")
+            args = listOf<String>("log", "--oneline", "--pretty=format:(#%h) %cn %s", "release/${android.defaultConfig.versionName}..origin/develop")
 //            args = listOf<String>("log", "--merges", "--oneline", "--pretty=format:(#%h) %cn %s", "release/${android.defaultConfig.versionName}")
 //            args = listOf<String>("log", "--pretty=format:(#%h) %cn %s")
             standardOutput = os
