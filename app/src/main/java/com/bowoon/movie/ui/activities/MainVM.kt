39bcffd78d1f4cc1338d85b4a1afa9a9e60ea400
bowoon <haioer@naver.com>
2026-01-19 13:37:53 +0900

    MainVM에서 DataManager 참조하는 부분 수정

--- app/src/main/java/com/bowoon/movie/ui/activities/MainVM.kt
+++ app/src/main/java/com/bowoon/movie/ui/activities/MainVM.kt
@@ -2,14 +2,11 @@ package com.bowoon.movie.ui.activities
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.bowoon.common.Result
-import com.bowoon.common.asResult
 import com.bowoon.data.repository.DatabaseRepository
 import com.bowoon.data.repository.UserDataRepository
 import com.bowoon.data.util.DataManager
+import com.bowoon.data.util.MovieAppDataState
 import com.bowoon.data.util.SyncManager
-import com.bowoon.model.DarkThemeConfig
-import com.bowoon.model.MovieAppData
 import com.bowoon.ui.image.imageUrl
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.SharingStarted
@@ -39,15 +36,8 @@ class MainVM @Inject constructor(
     }
 
     val movieAppData = dataManager.movieAppData
-        .onEach { imageUrl = it.getImageUrl() }
-        .asResult()
-        .map {
-            when (it) {
-                is Result.Loading -> MovieAppDataState.Loading
-                is Result.Success -> MovieAppDataState.Success(data = it.data)
-                is Result.Error -> MovieAppDataState.Error(throwable = it.throwable)
-            }
-        }.stateIn(
+        .onEach { imageUrl = it.getMovieAppData().getImageUrl() }
+        .stateIn(
             scope = viewModelScope,
             started = SharingStarted.Eagerly,
             initialValue = MovieAppDataState.Loading
@@ -60,23 +50,4 @@ class MainVM @Inject constructor(
             started = SharingStarted.Lazily,
             initialValue = emptyList()
         )
-}
-
-sealed interface MovieAppDataState {
-    data object Loading : MovieAppDataState
-    data class Success(val data: MovieAppData) : MovieAppDataState {
-        override fun shouldUseDarkTheme(isSystemDarkTheme: Boolean) =
-            when (data.isDarkMode) {
-                DarkThemeConfig.FOLLOW_SYSTEM -> isSystemDarkTheme
-                DarkThemeConfig.LIGHT -> false
-                DarkThemeConfig.DARK -> true
-            }
-
-        override fun getMovieAppData(): MovieAppData = this.data
-    }
-    data class Error(val throwable: Throwable) : MovieAppDataState
-
-    fun shouldKeepSplashScreen() = this is Loading
-    fun shouldUseDarkTheme(isSystemDarkTheme: Boolean): Boolean = isSystemDarkTheme
-    fun getMovieAppData(): MovieAppData = MovieAppData()
 }
