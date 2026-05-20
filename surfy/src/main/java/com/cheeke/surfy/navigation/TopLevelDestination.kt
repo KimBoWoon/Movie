80ef1c12c748f65e8d425527259d5e5d7acab3e5
KimBoWoon <haioer@naver.com>
2026-05-20 13:39:52 +0900

    TopLevelDestination 수정

--- surfy/src/main/java/com/cheeke/surfy/navigation/TopLevelDestination.kt
+++ surfy/src/main/java/com/cheeke/surfy/navigation/TopLevelDestination.kt
@@ -13,8 +13,8 @@ import com.slack.circuit.runtime.screen.Screen
 enum class TopLevelDestination(
     val selectedIcon: ImageVector,
     val unselectedIcon: ImageVector,
-    @StringRes val iconTextId: Int,
-    @StringRes val titleTextId: Int,
+    @param:StringRes val iconTextId: Int,
+    @param:StringRes val titleTextId: Int,
     val screen: Screen,
     val rootTab: RootTab
 ) {
@@ -26,13 +26,6 @@ enum class TopLevelDestination(
         screen = HomeScreen,
         rootTab = RootTab.HOME
     ),
-//    SEARCH(
-//        selectedIcon = Icons.Rounded.Search,
-//        unselectedIcon = Icons.Outlined.Search,
-//        iconTextId = com.bowoon.movie.feature.search.R.string.feature_search_name,
-//        titleTextId = com.bowoon.movie.feature.search.R.string.feature_search_name,
-//        screen = Search
-//    ),
     FAVORITE(
         selectedIcon = Icons.Rounded.Favorite,
         unselectedIcon = Icons.Outlined.FavoriteBorder,
@@ -40,49 +33,5 @@ enum class TopLevelDestination(
         titleTextId = com.cheeke.surfy.feature.favorite.R.string.feature_favorite_name,
         screen = FavoriteScreen(),
         rootTab = RootTab.FAVORITE
-    ),
-//    MY(
-//        selectedIcon = Icons.Rounded.Settings,
-//        unselectedIcon = Icons.Outlined.Settings,
-//        iconTextId = com.bowoon.movie.feature.my.R.string.feature_my_name,
-//        titleTextId = com.bowoon.movie.feature.my.R.string.feature_my_name,
-//        screen = My
-//    )
+    )
 }
-
-/**
- * 앱 최상단 내비게이션 바
- */
-//data class TopLevelDestination(
-//    val selectedIcon: ImageVector,
-//    val unselectedIcon: ImageVector,
-//    @param:StringRes val titleTextId: Int,
-//    val type: Class<out NavKey>
-//)
-//
-//val HOME = TopLevelDestination(
-//    selectedIcon = Icons.Rounded.Home,
-//    unselectedIcon = Icons.Outlined.Home,
-//    titleTextId = R.string.feature_home_name,
-//    type = HomeNavKey::class.java
-//)
-//
-//val FAVORITE = TopLevelDestination(
-//    selectedIcon = Icons.Rounded.Favorite,
-//    unselectedIcon = Icons.Outlined.FavoriteBorder,
-//    titleTextId = com.cheeke.surfy.feature.favorite.R.string.feature_favorite_name,
-//    type = FavoriteNavKey::class.java
-//)
-//
-////val MY = TopLevelDestination(
-////    selectedIcon = Icons.Rounded.Settings,
-////    unselectedIcon = Icons.Outlined.Settings,
-////    titleTextId = com.cheeke.surfy.feature.my.R.string.feature_my_name,
-////    type = SettingNavKey::class.java
-////)
-//
-//val TOP_LEVEL_NAV_ITEMS = mapOf(
-//    HomeNavKey to HOME,
-//    FavoriteNavKey(tab = 0) to FAVORITE,
-////    SettingNavKey to MY
-//)
